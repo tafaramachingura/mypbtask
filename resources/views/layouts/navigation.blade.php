591d2423e173491119aa605dc9b352a6cd508063
tafaramachingura <tafaramachingura@gmail.com>
2024-06-27 15:14:36 +0200

    nav changes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,9 @@
+<style>
+
+.bg-blue-500{
+    background-color: #0265cb !important;
+}
+</style>
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
